c5e40513617a390b017da06a3b395d09a4ada9b7
Sanko <sanko.kf.dev@gmail.com>
2026-06-11 17:05:13 +0200

    add new levels

--- src/ch/hevs/gdx2d/game/Ascent.scala
+++ src/ch/hevs/gdx2d/game/Ascent.scala
@@ -6,7 +6,7 @@ import com.badlogic.gdx.graphics.Color
 
 class Ascent extends PortableApplication(1920, 1080) {
 
-  val levelFiles = Array("level1.txt", "level2.txt", "level3.txt", "level4.txt")
+  val levelFiles = Array("level1.txt", "level2.txt", "level3.txt", "level4.txt", "level5.txt", "level6.txt")
   var currentLevel = 0
 
   var player: Player = null
